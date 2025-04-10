133b95833e3fa920b5d20899ca0517902c64915b
nimboo-zrad <nimazr800@gmail.com>
2025-04-10 16:37:32 +0330

    added multiple forms and sliding

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
-console.log('hi');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".form");
+  const [one, two, three, four] = document.querySelectorAll('.page');
+  const [submitOne, submitTwoBack, submitTwoNext, submitThreeBack, submitThreeNext, submitFinal] = document.querySelectorAll(".submit");
+  const addElem = document.querySelector(".addElem");
+  submitOne.addEventListener("click", (event)=>{
+  	event.preventDefault();
+      if(form.checkValidity()){
+      	one.style.opacity = 0;
+          two.style.opacity = 1;
+      }
+      else console.log(form.reportValidity());
+  });
+  
+  addElem.addEventListener("click", ()=>{
+  	const inputArea = document.createElement("input");
+      inputArea.type = "textarea";
+      inputArea.className = "area";
+ });
+});
+
+
